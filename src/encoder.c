622d9fd002d6122b5ccb2e1e32f9cfb8d750ae9b
Néo Foderé de Frutos <nfoderedefrutos@gmail.com>
2026-01-28 04:11:22 +0100

    Update encoder.c

--- src/encoder.c
+++ src/encoder.c
@@ -7,7 +7,7 @@
    \ \__\   \ \__/ /     \ \__\ \__\/___/\__\  
     \|__|    \|__|/       \|__|\|__/___/ /\ _\ 
     
-    FVAX and all related titles and logos are trademarks of FODSOFT™ (Néo Foderé de Frutos). All rights reserved.
+    FVAX and all related titles and logos are trademarks of FODSOFT(TM). Neo Fodere de Frutos. All rights reserved.
     FVAX is licensed under the AGPLv3. More information at "https://github.com/neofodere/fvax/blob/main/LICENSE".
 */
 
