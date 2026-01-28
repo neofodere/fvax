4bb4b67b3e57c71f22962451c38bd922360977ea
Néo Foderé de Frutos <nfoderedefrutos@gmail.com>
2026-01-28 01:49:42 +0100

    Update fvax.c

--- src/fvax.c
+++ src/fvax.c
@@ -1,11 +1,11 @@
 /*
- ________ ___      ___ ________     ___    ___ 
-|\  _____\\  \    /  /|\   __  \   |\  \  /  /|
-\ \  \__/\ \  \  /  / | \  \|\  \  \ \  \/  / /
- \ \   __\\ \  \/  / / \ \   __  \  \ \    / / 
-  \ \  \_| \ \    / /   \ \  \ \  \  /     \/  
-   \ \__\   \ \__/ /     \ \__\ \__\/___/\__\  
-    \|__|    \|__|/       \|__|\|__/___/ /\ _\ 
+    ________ ___      ___ ________     ___    ___ 
+   |\  _____\\  \    /  /|\   __  \   |\  \  /  /|
+   \ \  \__/\ \  \  /  / | \  \|\  \  \ \  \/  / /
+    \ \   __\\ \  \/  / / \ \   __  \  \ \    / / 
+     \ \  \_| \ \    / /   \ \  \ \  \  /     \/  
+      \ \__\   \ \__/ /     \ \__\ \__\/___/\__\  
+       \|__|    \|__|/       \|__|\|__/___/ /\ _\ 
     
     FVAX and all related titles and logos are trademarks of FODSOFT™ (Néo Foderé de Frutos). All rights reserved.
     FVAX is licensed under the AGPLv3. More information at "https://github.com/neofodere/fvax/blob/main/LICENSE".
